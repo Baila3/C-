19e3731b216eca2dc6447b7de4f44566d3d84f7d
Baila3 <bailaly16@gmail.com>
2023-01-11 23:03:43 -0500

    learn variables in c++

---
+++ variable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int year;
+int main()
+{
+  double miles;
+  double kilometers;
+
+  std::cout << "Distance in miles: ";
+  std::cin >> miles;
+
+  kilometers = miles * 1.609344;
+
+  std::cout << "There are " << kilometers << " kilometers in " << miles << " miles\n";
+}
